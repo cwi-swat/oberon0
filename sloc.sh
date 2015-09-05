7bed47c2098c7335a6b47c3bed1329795211b53f
Tijs van der Storm <storm@cwi.nl>
2015-09-05 16:40:38 +0200

    Sloc script

---
+++ sloc.sh
@@ -0,0 +1,31 @@
+echo "*** Normalize ***"
+
+cloc src/ldta/oberon0/l4/normalize
+
+echo "**** Java ****"
+
+cloc src/ldta/oberon0/l4/tojava/Oberon0ToJava.rsc
+
+echo "**** Bytecode ****"
+
+cloc  src/ldta/oberon0/l4/tojava/Oberon0ToJavaBytecode.rsc
+
+
+
+echo "**** EVAL l1 ****"
+
+cloc src/ldta/oberon0/l1/eval
+
+echo "**** EVAL l3 ****"
+
+cloc src/ldta/oberon0/l3/eval
+
+echo "**** EVAL l4 ****"
+cloc src/ldta/oberon0/l4/eval
+
+echo "**** EVAL total ****"
+
+cloc src/ldta/oberon0/l1/eval src/ldta/oberon0/l3/eval src/ldta/oberon0/l4/eval
+
+
+
